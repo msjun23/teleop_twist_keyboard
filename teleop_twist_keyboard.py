0ce55f9372ab94167fb75894b32a65876e4663c3
msjun23 <msjun23@gmail.com>
2021-08-02 17:00:57 +0900

    Customize for jetbot

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -19,6 +19,19 @@ Moving around:
    j    k    l
    m    ,    .
 
+anything else : stop
+
+CTRL-C to quit
+"""
+
+'''msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+
 For Holonomic mode (strafing), hold down the shift key:
 ---------------------------
    U    I    O
@@ -35,37 +48,38 @@ w/x : increase/decrease only linear speed by 10%
 e/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
-"""
+"""'''
 
 moveBindings = {
+        'u':(1,0,0,1),
         'i':(1,0,0,0),
         'o':(1,0,0,-1),
         'j':(0,0,0,1),
         'l':(0,0,0,-1),
-        'u':(1,0,0,1),
+        'm':(-1,0,0,-1),
         ',':(-1,0,0,0),
         '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
+        #'O':(1,-1,0,0),
+        #'I':(1,0,0,0),
+        #'J':(0,1,0,0),
+        #'L':(0,-1,0,0),
+        #'U':(1,1,0,0),
+        #'<':(-1,0,0,0),
+        #'>':(-1,-1,0,0),
+        #'M':(-1,1,0,0),
+        #'t':(0,0,1,0),
+        #'b':(0,0,-1,0),
     }
 
-speedBindings={
+speedBindings=(1.1, 1.1)
+'''speedBindings={
         'q':(1.1,1.1),
         'z':(.9,.9),
         'w':(1.1,1),
         'x':(.9,1),
         'e':(1,1.1),
         'c':(1,.9),
-    }
+    }'''
 
 class PublishThread(threading.Thread):
     def __init__(self, rate):
@@ -181,6 +195,8 @@ if __name__=="__main__":
     th = 0
     status = 0
 
+    pre_key = 'k'
+
     try:
         pub_thread.wait_for_subscribers()
         pub_thread.update(x, y, z, th, speed, turn)
@@ -190,18 +206,19 @@ if __name__=="__main__":
         while(1):
             key = getKey(key_timeout)
             if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
-
-                print(vels(speed,turn))
-                if (status == 14):
-                    print(msg)
-                status = (status + 1) % 15
+                if key == pre_key:
+                    # In same key command
+                    # change spped or turn
+                    speed = speed * speedBindings[0]
+                    turn = turn * speedBindings[1]
+                else :
+                    x = moveBindings[key][0]
+                    y = moveBindings[key][1]
+                    z = moveBindings[key][2]
+                    th = moveBindings[key][3]
+                
+                print(x, y, z, th)
+                print(vels(speed, turn))
             else:
                 # Skip updating cmd_vel if key timeout and robot already
                 # stopped.
@@ -211,10 +228,20 @@ if __name__=="__main__":
                 y = 0
                 z = 0
                 th = 0
+                speed = 0.5
+                turn = 1.0
                 if (key == '\x03'):
                     break
- 
+            '''elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+
+                print(vels(speed,turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15'''
             pub_thread.update(x, y, z, th, speed, turn)
+            pre_key = key
 
     except Exception as e:
         print(e)
