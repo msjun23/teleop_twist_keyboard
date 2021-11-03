0bcc747c3d431c11acd7a3da4d54f5b469e9400a
msjun23 <msjun23@gmail.com>
2021-11-03 21:44:24 +0900

    Upload pub_keyboard_input.py

---
+++ pub_keyboard_input.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import threading
+
+import roslib; roslib.load_manifest('teleop_twist_keyboard')
+import rospy
+
+from std_msgs.msg import String
+
+import sys, select, termios, tty
+
+
+def getKey(key_timeout):
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+if __name__=="__main__":
+    settings = termios.tcgetattr(sys.stdin)
+
+    rospy.init_node('pub_keyboard_input')
+    publisher = rospy.Publisher('key_input', String, queue_size = 1)
+
+    key_timeout = rospy.get_param("~key_timeout", 0.0)
+    if key_timeout == 0.0:
+        key_timeout = None
+
+    pub_str = ''
+
+    try:
+        while(1):
+            key = getKey(key_timeout)
+            
+            if (key == ''):
+                continue
+            elif (key == '\x03'):
+                break
+            elif (key == '\r'):
+                publisher.publish(pub_str)
+                pub_str = ''
+            else:
+                pub_str += key
+                print(key)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
